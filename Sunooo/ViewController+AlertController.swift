8ae590c6b28b4333240a0cb6c494b148d324a779
kwazzar <fugitiwe@gmail.com>
2022-05-07 23:58:13 +0300

    preview

---
+++ Sunooo/ViewController+AlertController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController+AlertController.swift
+//  Sunooo
+//
+//  Created by Quasar on 07.05.2022.
+//
+
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        ac.addTextField { tf in
+            let cities = ["San Francisco", "Kyiv", "New York", "Stambul", "Viena"]
+            tf.placeholder = cities.randomElement()
+        }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = ac.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        ac.addAction(search)
+        ac.addAction(cancel)
+        present(ac, animated: true, completion: nil)
+    }
+}
